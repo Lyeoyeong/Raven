c4b34c1ed2e2455d8fa16397a4c03e1e4804a325
Ryeoyeong <liying.jin@bnc.ox.ac.uk>
2022-05-04 18:15:16 +0100

    Some Exercise

--- R4DS/R4DS.R
+++ R4DS/R4DS.R
@@ -4,6 +4,8 @@ library(nycflights13)
 library(tidyverse)
 
 view(flights)
+
+# Filter
 # Exercise
 filter(flights, arr_delay > 2)
 filter(flights, dest %in% c('IAH','HOU'))
@@ -12,3 +14,13 @@ filter(flights, between(distance, 1000, 2000))
 length(filter(flights, is.na(dep_time)))
 NA^0
 ?between()
+
+# Arrange
+# Exercise
+df <- tibble(x = c(5,2,NA))
+arrange(df,x)
+arrange(df, desc(is.na(x)))
+is.na(x)
+x <- c(5,2,NA)
+
+arrange(flights, air_time)
